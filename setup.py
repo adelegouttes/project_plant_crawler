caf7989fe9612b05d6d371aeb6e4c9755397c10c
agouttes_zalando <adele.gouttes@zalando.de>
2019-02-18 22:28:03 +0100

    Create setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+REQUIREMENTS = [
+      'requests==2.21.0',
+      'beautifulsoup4==4.7.1',
+      'flask==1.0.2',
+      'sqlalchemy==1.2.18',
+]
+
+setup(name='project_plant_crawler',
+      version='0.1',
+      description='Organise your vegetable garden',
+      url='https://github.com/adelegouttes/project_plant_crawler',
+      author='Adele Gouttes',
+      install_requires=REQUIREMENTS,
+      )
