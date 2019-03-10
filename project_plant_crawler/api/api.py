919a97b54dfe97d796e67c9cafae3a158bfa3e95
agouttes_zalando <adele.gouttes@zalando.de>
2019-03-10 14:48:12 +0100

    Api: filter harvest month endpoint

--- project_plant_crawler/api/api.py
+++ project_plant_crawler/api/api.py
@@ -68,7 +68,7 @@ def api_plant_filter():
     plants = []
     for plant in db_session.query(Plant).filter(to_filter).all():
         plants.append(plant.jsonify())
-    if len(plants)==0:
+    if len(plants) == 0:
         return page_not_found(404,
                               message='<p>Filtering is too restrictive: no plant matches all filters.</p>')
     return jsonify(plants)
@@ -84,7 +84,7 @@ def api_month_all():
 
 
 @app.route('/api/v1/resources/months/harvest_months/all', methods=['GET'])
-def api_month_harvest_all():
+def api_month_harvest_month_all():
     """A route to return all of the plants gathered by harvest month."""
     harvest_months = []
     for month in db_session.query(Month).all():
@@ -94,6 +94,37 @@ def api_month_harvest_all():
         for plant in db_session.query(Plant).filter(Plant.harvest_months.contains(month)).all():
             harvest_month['plants'].append(plant.jsonify())
         harvest_months.append(harvest_month)
+    return jsonify(harvest_months)
+
+
+@app.route('/api/v1/resources/months/harvest_months', methods=['GET'])
+def api_month_harvest_month_filter():
+    """A route to return all of the plants gathered for one harvest month."""
+
+    query_parameters = request.args.to_dict()  # Get all arguments in the filter of the url
+
+    allowed_keys = ['month_id', 'month_name_eng']
+    try:
+        to_filter = generate_filter_string(allowed_keys=allowed_keys,
+                                           query_parameters=query_parameters)
+    except KeyError as e_info:
+        return page_not_found(404, message=e_info.args[0])
+
+    harvest_months = []
+    for month in db_session.query(Month).filter(to_filter).all():
+        harvest_month = dict()
+        harvest_month['month'] = month.jsonify()
+        harvest_month['plants'] = []
+        for plant in db_session.query(Plant).filter(Plant.harvest_months.contains(month)).all():
+            harvest_month['plants'].append(plant.jsonify())
+        if len(harvest_month['plants']) == 0:
+            return page_not_found(404,
+                                  message='<p>Filtering is too restrictive: no plants for this harvest month.</p>')
+        harvest_months.append(harvest_month)
+    if len(harvest_months) == 0:
+        return page_not_found(404,
+                              message='<p>Filtering is too restrictive: no harvest month for filter.</p>')
+
     return jsonify(harvest_months)
 
 
